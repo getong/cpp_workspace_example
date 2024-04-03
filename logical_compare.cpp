2022c0a99d228907f90108a9bd93e33004b1d4f6
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-04-03 21:32:39 +0800

    add logical compare

---
+++ logical_compare.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  int x = 50;
+
+  if (x > 5 && x < 10) {
+    cout << x << " is an integer between 5 and 10." << endl;
+  } else {
+    cout << x << " is not an integer between 5 and 10." << endl;
+  }
+
+  if (x > 5 and x < 10) {
+    cout << x << " is an integer between 5 and 10." << endl;
+  } else {
+    cout << x << " is not an integer between 5 and 10." << endl;
+  }
+
+  // if (5 < x < 10) {
+  //   cout << x << " is an integer between 5 and 10." << endl;
+  // } else {
+  //   cout << x << " is not an integer between 5 and 10." << endl;
+  // }
+
+  return 0;
+}
+
+// g++ -std=c++11 logical_compare.cpp -o a.out
