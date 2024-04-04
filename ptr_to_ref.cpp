1596e42e41feefb0c59a9551f9b959acaa4d5ab3
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-04-04 11:47:42 +0800

    add pointer to reference

---
+++ ptr_to_ref.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  int var{55};
+  int *ptr = &var;
+
+  // Pointer to Pointer -> ptrToPtr stores the address of ptr
+  int **ptrToPtr = &ptr;
+
+  // Reference to Pointer -> refToPtr is an Alias of ptr
+  int *&refToPtr = ptr;
+
+  // refToPtr can be add number, but is wrong
+  // refToPtr += 5;
+  // cout << "refToPtr is " << refToPtr << endl;
+
+  *refToPtr += 5;
+  cout << "refToPtr is " << *refToPtr << endl;
+  cout << "var is " << var << endl;
+  return 0;
+}
+
+// g++ -std=c++11 ptr_to_ref.cpp
