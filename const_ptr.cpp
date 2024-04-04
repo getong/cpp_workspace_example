5a3b41f55fcbd0c5cbc7340216bb3adacbb96855
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-04-04 12:06:32 +0800

    add const pointer example

---
+++ const_ptr.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  int var{55}, varToAssign{250};
+  int *ptr = &var;
+
+  int *const &refToPtr = ptr;
+  // can not be reassigned
+  // refToPtr = &varToAssign;
+  cout << "refToPtr is " << *refToPtr << endl;
+
+  int *&refToPtr2 = ptr;
+  // without const, can be reassigned
+  refToPtr2 = &varToAssign;
+  cout << "refToPtr2 is " << *refToPtr2 << endl;
+
+  return 0;
+}
+
+// g++ -std=c++11 const_ptr.cpp
