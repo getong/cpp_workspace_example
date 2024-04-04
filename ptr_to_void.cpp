1c582033de828d2d0cb900902eafa296ea2b2114
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-04-04 21:06:12 +0800

    add pointer to void

---
+++ ptr_to_void.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+  void *p;
+
+  double d = 10;
+  p = &d;
+
+  // Correct way to dereference a void pointer
+  cout << "*p is " << *(static_cast<double *>(p)) << endl;
+  return 0;
+}
+
+// g++ -std=c++11 ptr_to_void.cpp
